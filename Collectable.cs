774f490b5fd0bb2bd037a280702ea67f5cf19b98
Nick0108 <zidonglin@outlook.com>
2017-08-29 19:42:24 +0800

    Feat: Build a collection function

---
+++ Collectable.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collectable : MonoBehaviour {
+
+    public Canvas ReadableContent;
+    public Canvas HearableContent;
+    //judge the OBJ's attribute
+    public bool isNecessary = false;
+    public bool hasReadCanvas = false;
+    public bool hasVoice = false;
+    public bool hasEvent = false;
+    public bool CanHold = false;
+
+    public bool isCollected = false;
+    //public bool isRead = false;
+    public bool isHeard = false;
+    public bool EventNowHappen = false;
+
+    private AudioSource AudioSou;
+
+    private void Start()
+    {
+        if (hasVoice)
+        {
+            AudioSou = GetComponent<AudioSource>();
+        }
+        if (hasReadCanvas && ReadableContent == null) 
+        {
+            ReadableContent = GetComponentInChildren<Canvas>();
+            ReadableContent.gameObject.SetActive(false);
+        }
+    }
+
+    public void Read()
+    {
+        if (!isCollected)
+        {
+            if(isNecessary)
+                GameManager.collectionNum++;
+            isCollected = true;
+            Debug.Log("collectionNum:"+ GameManager.collectionNum);
+        }
+        if (hasReadCanvas)
+        {
+            ReadableContent.gameObject.SetActive(true);
+        }
+        if (hasVoice)
+        {
+            if (!isHeard)
+            {
+                AudioSou.Play();
+                isHeard = true;
+                HearableContent.gameObject.SetActive(true);
+                StartCoroutine(AudioContent());
+            }
+        }
+    }
+
+    public void Release()
+    {
+        if (hasEvent)
+        {
+            EventNowHappen = true;
+        }
+        if (hasReadCanvas)
+        {
+            ReadableContent.gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator AudioContent()
+    {
+        yield return new WaitForSeconds(5);
+        HearableContent.gameObject.SetActive(false);
+    }
+}
---
+++ GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+using UnityEngine.Video;
+
+public class GameManager : MonoBehaviour {
+    
+    public static int collectionNum;
+    public GameObject player;
+
+	void Start () {
+        collectionNum = 0;
+    }
+}
