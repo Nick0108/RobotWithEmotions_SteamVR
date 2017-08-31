a6b3d21d254fd7b22a42cd093eb62a3ac8df0006
Nick0108 <zidonglin@outlook.com>
2017-08-31 12:16:00 +0800

    Feat: Set Action1&2 playable

---
+++ Action1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+
+public class Action1 : MonoBehaviour {
+    public PlayableDirector UTCplayable;
+    public Readable startbook;
+    public GameObject Happyball;
+    public bool Get = false;
+    public GameObject UTC;
+
+    void Update () {
+        if (startbook.EventNowHappen)
+        {
+            UTCplayable.Play();
+            startbook.EventNowHappen = false;
+        }
+        if (Get)
+            UTCplayable.Resume();
+        else if (UTCplayable.time > 12.42)
+        {
+            UTCplayable.Pause();
+            Happyball.SetActive(true);
+        }
+            
+        if (UTCplayable.time > 19.15)
+        {
+            UTCplayable.Stop();
+            UTC.SetActive(false);
+        }
+    }
+
+    
+}
---
+++ Action2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+    public class Action2 : MonoBehaviour {
+    public PlayableDirector UTCplayable;
+    public GameObject UTC;
+    public bool start = false;
+    private bool hasPlay = false;
+
+
+    private void Update()
+    {
+        if (!hasPlay)
+        {
+            if (start)
+            {
+                UTCplayable.Play();
+                hasPlay = true;
+            } 
+        }
+        if (UTCplayable.time > 8.8)
+            UTC.SetActive(false);
+    }
+}
