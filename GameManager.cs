fa54b059ec4ed601ab64de1a44eff3648749532d
Nick0108 <zidonglin@outlook.com>
2017-09-10 20:38:27 +0800

    Fix: make the different timeline can switch

--- GameManager.cs
+++ GameManager.cs
@@ -7,46 +7,81 @@ using UnityEngine.Video;
 
 public class GameManager : MonoBehaviour {
     
+	public enum Chapter
+    {
+        Chapter1,
+        Chapter2,
+        Chapter3,
+        Chapter4,
+        Chapter5,
+        Chapter6,
+        Chapter7,
+    }
     public static int collectionNum;
+    public static Chapter currentChapter;
     public GameObject player;
+    private SteamVR_LoadLevel LoadLevel;
 
-	//action1
-	public PlayableDirector UTCplayable;
-    public Readable startbook;
+    //for testing
+    public Chapter ChangeChapter;
+    public int killEnemyNum;
+    public bool testing = false; 
+
+    //Chapter1
+    public PlayableDirector UTCplayable01;
+    public Collectable startbook;
     public GameObject Happyball;
-    public bool Get = false;
-    public GameObject UTC;
+    public bool GetHappyBall = false;
+    public GameObject UTC01;
 
-	//action2
-	public class Action2 : MonoBehaviour {
-    public PlayableDirector UTCplayable;
-    public GameObject UTC;
-    public bool start = false;
+    //Chapter2
+    public PlayableDirector UTCplayable02;
+    public GameObject UTC02;
+    public bool start02 = false;
     private bool hasPlay = false;
+    public DoorOpen door;
+    public DoorOpen door2;
 
-	//action3
-	public PlayableDirector UTCplayable;
-    public GameObject UTC;
+    //Chapter3
+    public ChangeSkybox changeSkybox;
+    public PlayableDirector UTCplayable03;
+    public GameObject UTC03;
     public GameObject UTCObject;
     public Shader TransparentShader;
-
+    public GameObject fearBall;
+    public bool isChangeWeather = false;
+    public bool isChangeWeather2 = false;
+    public Collectable lamp;
+    public DoorOpen PrincessDoor;
+    public DoorOpen PrincessDoor2;
     public bool BeginToCry = false;
     public bool NotCry = false;
     public bool Disappear = false;
-
     public GameObject Elephant;
-
+    public bool GetFearBall = false;
     private SkinnedMeshRenderer[] meshRenders;
+    public AudioClip[] chapter3AudioClips;
+    public AudioSource chapter3AudioSource;
+    private float ToVoice2Timer;
+    private bool isCp3Audio1Play = false;
+    private bool isNotCry = false;
+    private bool countTo2 = false;
 
 	void Start () {
         collectionNum = 0;
-		meshRenders = UTCObject.GetComponentsInChildren<SkinnedMeshRenderer>();
+        currentChapter = Chapter.Chapter1;
+        LoadLevel = gameObject.GetComponent<SteamVR_LoadLevel>();
+        //Chapter03
+        meshRenders = UTCObject.GetComponentsInChildren<SkinnedMeshRenderer>();
+        CombineTimer = 0;
+        trueEndTimer = 0;
+        ToVoice2Timer = 0;
     }
 
 
 	void update(){
-		//action1
-		if (currentChapter == Chapter.Chapter1)
+		//Chapter01
+        if (currentChapter == Chapter.Chapter1)
         {
             if (startbook.EventNowHappen)
             {
@@ -68,8 +103,8 @@ public class GameManager : MonoBehaviour {
                 currentChapter = Chapter.Chapter2;
             }
         }
-		//action2
-		if (currentChapter == Chapter.Chapter2)
+        //Chapter02
+        if (currentChapter == Chapter.Chapter2)
         {
             if (door.isOpen || door2.isOpen)
             {
@@ -90,51 +125,90 @@ public class GameManager : MonoBehaviour {
                 currentChapter = Chapter.Chapter3;
             }
         }
-
-		//action3
-
-		if (currentChapter == Chapter.Chapter3)
-		{
-			
-			if (!isChangeWeather)
-			{
-				if (player.transform.position.x > 3f && player.transform.position.z < -15f)
-				{
-					changeSkybox.currentSkyBox = ChangeSkybox.skybox.Falling;
-					changeSkybox.CheckCurrentSky();
-					isChangeWeather = true;
-				}
-			}
-			
-			if (NotCry)
-			{
-				if (!isNotCry)
-				{
-					foreach (SkinnedMeshRenderer M in meshRenders)
-					{
-						M.material.shader = TransparentShader;
-					}
-					chapter3AudioSource.clip = chapter3AudioClips[2];
-					chapter3AudioSource.loop = true;
-					chapter3AudioSource.Play();
-					isNotCry = true;
-				}
-			}
-			if (Disappear)
-			{
-				UTC03.SetActive(false);
-				UTCplayable03.Stop();
-				Elephant.GetComponent<Rigidbody>().isKinematic = false;
-				fearBall.SetActive(true);
-				Disappear = false;
-				chapter3AudioSource.loop = false;
-				chapter3AudioSource.Stop();
-			}
-			if (GetFearBall)
-			{
-				TimeLine4.SetActive(true);
-				currentChapter = Chapter.Chapter4;
-			}
-		}
+        //Chapter03
+        if (currentChapter == Chapter.Chapter3)
+        {
+            if (!isChangeWeather2)
+            {
+                if (PrincessDoor.EventNowHappen || PrincessDoor2.EventNowHappen)
+                {
+                    changeSkybox.currentSkyBox = ChangeSkybox.skybox.Night;
+                    changeSkybox.CheckCurrentSky();
+                    isChangeWeather = true;
+                }
+            }
+            if (!isChangeWeather)
+            {
+                if (player.transform.position.x > 3f && player.transform.position.z < -15f)
+                {
+                    changeSkybox.currentSkyBox = ChangeSkybox.skybox.Falling;
+                    changeSkybox.CheckCurrentSky();
+                    isChangeWeather = true;
+                }
+            }
+            if (lamp.EventNowHappen)
+            {
+                PrincessDoor.isLock = false;
+                PrincessDoor2.isLock = false;
+                UTCplayable03.Play();
+                lamp.EventNowHappen = false;
+                if (UTCObject.transform.localPosition != Vector3.zero)
+                {
+                    UTCObject.transform.localPosition = Vector3.zero;
+                    UTCObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+                }
+                if (!isCp3Audio1Play)
+                {
+                    chapter3AudioSource.clip = chapter3AudioClips[0];
+                    chapter3AudioSource.loop = false;
+                    chapter3AudioSource.Play();
+                    isCp3Audio1Play = true;
+                    countTo2 = true;
+                }
+            }
+            if (countTo2)
+            {
+                ToVoice2Timer += Time.deltaTime;
+                if (ToVoice2Timer > 18.5f)
+                {
+                    if (!chapter3AudioSource.isPlaying)
+                    {
+                        chapter3AudioSource.clip = chapter3AudioClips[1];
+                        chapter3AudioSource.loop = true;
+                        chapter3AudioSource.Play();
+                        countTo2 = false;
+                    }
+                }
+            }
+            if (NotCry)
+            {
+                if (!isNotCry)
+                {
+                    foreach (SkinnedMeshRenderer M in meshRenders)
+                    {
+                        M.material.shader = TransparentShader;
+                    }
+                    chapter3AudioSource.clip = chapter3AudioClips[2];
+                    chapter3AudioSource.loop = true;
+                    chapter3AudioSource.Play();
+                    isNotCry = true;
+                }
+            }
+            if (Disappear)
+            {
+                UTC03.SetActive(false);
+                UTCplayable03.Stop();
+                Elephant.GetComponent<Rigidbody>().isKinematic = false;
+                fearBall.SetActive(true);
+                Disappear = false;
+                chapter3AudioSource.loop = false;
+                chapter3AudioSource.Stop();
+            }
+            if (GetFearBall)
+            {
+                TimeLine4.SetActive(true);
+                currentChapter = Chapter.Chapter4;
+            }
+        }
 	}
 }
