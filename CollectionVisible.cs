a5a351e842c0217231d80a46801de54b5fc2e3ad
Nick0108 <zidonglin@outlook.com>
2017-09-19 20:46:26 +0800

    Feat: Let the collections can be pointed out

---
+++ CollectionVisible.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectionVisible : MonoBehaviour {
+
+    public MeshRenderer[] CollectionRenderers;
+
+    public void NightChangeCollectionMaterial()
+    {
+        foreach(MeshRenderer i in CollectionRenderers)
+        {
+            foreach(Material j in i.materials)
+            {
+                j.SetFloat("_RimBool", 1f);
+            }
+        }
+    }
+
+    public void DayChangeCollectionMaterial()
+    {
+        foreach (MeshRenderer i in CollectionRenderers)
+        {
+            foreach (Material j in i.materials)
+            {
+                j.SetFloat("_RimBool", 0);
+            }
+        }
+    }
+}
